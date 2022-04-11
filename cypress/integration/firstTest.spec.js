ea8009dc93d245e341bab6c2542ada9258a090f2
Weronika <weronika.rudowicz@gmail.com>
2022-04-11 21:23:43 +0200

    5-29 Cy.Intercept

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -1,13 +1,11 @@
 describe('Test with backend', () => {
     beforeEach('login to the app', () => {
-        cy.server()
-        cy.route('GET', '**/tags', 'fixture:tags.json')
+        cy.intercept({ method: 'Get', path: 'tags' }, { fixture: 'tags.json' })
         cy.loginToApplication()
     })
 
     it('Verify post request', () => {
-        cy.server()
-        cy.route('POST', '**/articles').as('postArticles')
+        cy.intercept('POST', '**/articles').as('postArticles')
 
         cy.contains('New Article').click()
         cy.get('[formcontrolname="title"]').type('This is a title')
@@ -18,7 +16,7 @@ describe('Test with backend', () => {
         cy.wait('@postArticles')
         cy.get('@postArticles').then(xhr => {
             console.log(xhr)
-            expect(xhr.status).to.equal(200)
+            expect(xhr.response.statusCode).to.equal(200)
             expect(xhr.request.body.article.body).to.equal('This is a body')
             expect(xhr.response.body.article.description).to.equal('This is a description')
         })
@@ -30,10 +28,10 @@ describe('Test with backend', () => {
             .and('contain', 'testing')
     })
 
-    it.only('tags', () => {
+    it('articles', () => {
 
-        cy.route('GET', '**/articles/feed*', '{"articles":[],"articlesCount":0}')
-        cy.route('GET', '**/articles*', 'fixture:articles.json')
+        cy.intercept('GET', '**/articles/feed*', { "articles": [], "articlesCount": 0 })
+        cy.intercept('GET', '**/articles*', { fixture: 'articles.json' })
         cy.contains('Global Feed').click()
         cy.get('app-article-list button').then(listOfButtons => {
             expect(listOfButtons[0]).to.contain('1')
@@ -42,10 +40,37 @@ describe('Test with backend', () => {
 
         cy.fixture('articles').then(file => {
             const articlelink = file.articles[1].slug
-            cy.route('POST', '**/articles/' + articlelink + '/favorite', file)
+            cy.intercept('POST', '**/articles/' + articlelink + '/favorite', file)
         })
         cy.get('app-article-list button').eq(1).click()
             .should('contain', '6')
+    })
+
+    it('Intercepting and modifying the request & response', () => {
+        // cy.intercept('POST', '**/articles', (req) => {
+        //     req.body.article.description = "This is a description 2"
+        // }).as('postArticles')
+
+        cy.intercept('POST', '**/articles', (req) => {
+            req.reply(res => {
+                expect(res.body.article.description).to.equal('This is a description')
+                res.body.article.description = "This is a description 2"
+            })
+        }).as('postArticles')
+
+        cy.contains('New Article').click()
+        cy.get('[formcontrolname="title"]').type('This is a title')
+        cy.get('[formcontrolname="description"]').type('This is a description')
+        cy.get('[formcontrolname="body"]').type('This is a body')
+        cy.contains('Publish Article').click()
+
+        cy.wait('@postArticles')
+        cy.get('@postArticles').then(xhr => {
+            console.log(xhr)
+            expect(xhr.response.statusCode).to.equal(200)
+            expect(xhr.request.body.article.body).to.equal('This is a body')
+            expect(xhr.response.body.article.description).to.equal('This is a description 2')
+        })
     })
 
 })
