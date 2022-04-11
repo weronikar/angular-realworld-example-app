47318ff461f28911d01c5fc1df3b6480e5a61d1b
Weronika <weronika.rudowicz@gmail.com>
2022-04-11 23:04:31 +0200

    6-34 Retries for runMode & openMode

--- cypress/integration/secondTest.spec.js
+++ cypress/integration/secondTest.spec.js
@@ -6,6 +6,6 @@ describe('Test log out', () => {
     it('verify user can log out successfully', () => {
         cy.contains('Settings').click()
         cy.contains('Or click here to logout').click()
-        cy.get('.navbar-nav').should('contain', 'Sign up')
+        cy.get('.navbar-nav').should('contain', 'Sign up1')
     })
 })
