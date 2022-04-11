213971c8e0f8e117d72c62fc0596cc856b8f8234
Weronika <weronika.rudowicz@gmail.com>
2022-04-11 19:46:42 +0200

    5-27 Browser API calls

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -4,6 +4,22 @@ describe('Test with backend', () => {
     })
 
     it.only('login test', () => {
-        cy.log('Should be logged in')
+        cy.server()
+        cy.route('POST', '**/articles').as('postArticles')
+
+        cy.contains('New Article').click()
+        cy.get('[formcontrolname="title"]').type('This is a title')
+        cy.get('[formcontrolname="description"]').type('This is a description')
+        cy.get('[formcontrolname="body"]').type('This is a body')
+        cy.contains('Publish Article').click()
+
+        cy.wait('@postArticles')
+        cy.get('@postArticles').then(xhr => {
+            console.log(xhr)
+            expect(xhr.status).to.equal(200)
+            expect(xhr.request.body.article.body).to.equal('This is a body')
+            expect(xhr.response.body.article.description).to.equal('This is a description')
+
+        })
     })
 })
