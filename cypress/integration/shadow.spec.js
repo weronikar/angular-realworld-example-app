84ba8c797b0bf06f630d73aebac2429bb1d420c3
weronikar <37331364+weronikar@users.noreply.github.com>
2022-08-24 22:11:47 +0200

    6-40 Shadow DOM

---
+++ cypress/integration/shadow.spec.js
@@ -0,0 +1,8 @@
+const { createYield } = require("typescript")
+
+describe('shadow dom', () => {
+    it('access shadow dom', () => {
+        cy.visit('https://radogado.github.io/shadow-dom-demo/')
+        cy.get('#app').shadow().find('#container')
+    })
+})
